915becc853a0ea6ede63e34fb072a8b2454ec91d
Jamie <jp2081@wildcats.unh.edu>
2017-08-06 20:24:21 -0700

    added grouping graph for species mingling

--- spatial-analysis-plots/spatial_data_graphs.r
+++ spatial-analysis-plots/spatial_data_graphs.r
@@ -22,3 +22,17 @@ minglingplot = minglingplot + labs(title='Species mingling as a function of spec
 minglingplot = minglingplot + geom_text(x=3, y=0.75, label=lm_eqn(data.frame(datatable$Mingling,datatable$Species.Richness)), parse = TRUE)
 
 minglingplot
+
+# with groupings
+publictable = datatable[datatable$Public.Land=='Y',]
+privatetable = datatable[datatable$Public.Land=='N',]
+
+minglinggroup = ggplot(datatable, aes(x=Species.Richness, y=Mingling, color=Public.Land))
+minglinggroup = minglinggroup + geom_point() + geom_smooth(method=lm, formula = y ~ poly(x,2))
+minglinggroup = minglinggroup + theme(panel.background =  element_rect(fill = 'white', colour = 'red'), panel.grid.major = element_line(colour = 'black', linetype = 'dotted'))
+minglinggroup = minglinggroup + labs(title='Species mingling as a function of species richness', x='Species Richness', y='Species Mingling')
+minglinggroup = minglinggroup + geom_text(x=3, y=0.75, label=lm_eqn(data.frame(privatetable$Mingling,privatetable$Species.Richness)), parse = TRUE, aes(color="N", fill='white'))
+minglinggroup = minglinggroup + geom_text(x=3, y=0.83, label=lm_eqn(data.frame(publictable$Mingling,publictable$Species.Richness)), parse = TRUE, aes(color="Y", fill='white'))
+
+
+minglinggroup
