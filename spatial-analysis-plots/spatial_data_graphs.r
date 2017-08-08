7c82e3086d3252ad19df5434f9b38e0f1e3cc5a8
Jamie <jp2081@wildcats.unh.edu>
2017-08-07 21:27:06 -0700

    added additional graphs

--- spatial-analysis-plots/spatial_data_graphs.r
+++ spatial-analysis-plots/spatial_data_graphs.r
@@ -3,6 +3,14 @@ require(ggplot2)
 
 datatable = read.csv('C:\\Users\\Jamie\\Dropbox\\Thesis\\R\\tables\\JMP_spatial_data.csv')
 
+savepng = function(filename) {
+  
+  path = 'C:\\Users\\Jamie\\Dropbox\\Thesis\\graphics'
+  save_path = file.path(path, filename)
+  ggsave(save_path, width = 7.5, height = 5, dpi = 100)
+  
+}
+
 lm_eqn <- function(df){
   y <- df[,1]
   x <- df[,2]
@@ -23,6 +31,8 @@ minglingplot = minglingplot + geom_text(x=3, y=0.75, label=lm_eqn(data.frame(dat
 
 minglingplot
 
+savepng('minglingvsrich.png')
+
 # with groupings
 publictable = datatable[datatable$Public.Land=='Y',]
 privatetable = datatable[datatable$Public.Land=='N',]
@@ -36,3 +46,17 @@ minglinggroup = minglinggroup + geom_text(x=3, y=0.83, label=lm_eqn(data.frame(p
 
 
 minglinggroup
+
+savepng('minglinggroup.png')
+
+
+# mingling vs PINPON graph
+minglingpinpon = ggplot(datatable, aes(x=PINPON, y=Mingling)) + ylim(0,1)
+minglingpinpon = minglingpinpon + geom_point() + geom_smooth(method=lm, formula = y ~ poly(x,2))
+minglingpinpon = minglingpinpon + theme(panel.background =  element_rect(fill = 'white', colour = 'red'), panel.grid.major = element_line(colour = 'black', linetype = 'dotted'))
+minglingpinpon = minglingpinpon + labs(title='Species mingling as a function of species richness', x='Relative PINPON Abundance', y='Species Mingling')
+minglingpinpon = minglingpinpon + geom_text(x=0.8, y=0.8, label=lm_eqn(data.frame(datatable$Mingling,datatable$Species.Richness)), parse = TRUE)
+
+minglingpinpon
+
+savepng('minglingpinpon.png')
